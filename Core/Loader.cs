76414c1450894e06382a3303d4b6ed19ed68ed18
official-notfishvr <notfishvrvr+6545@gmail.com>
2026-01-05 12:39:48 -0600

    fix?

--- Core/Loader.cs
+++ Core/Loader.cs
@@ -2,7 +2,7 @@ using MelonLoader;
 using Mimesis_Mod_Menu.Core;
 using UnityEngine;
 
-[assembly: MelonInfo(typeof(Loader), "ModMenu", "2.6.0", "notfishvr")]
+[assembly: MelonInfo(typeof(Loader), "ModMenu", "2.6.1", "notfishvr")]
 [assembly: MelonGame("ReLUGames", "MIMESIS")]
 
 namespace Mimesis_Mod_Menu.Core
